bbef4351ace143e7de87a5bb585f5dfd0a0d8a75
srilekhavalmiki <srilekhagopanaboina1999@gmail.com>
2019-06-15 23:38:24 +0530

    firstapp

---
+++ myfirstapp/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { Test1Component } from './test1/test1.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    Test1Component
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
